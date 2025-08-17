4b7dbd5e0a4c8e9efc60223e273de4deb6fc0804
Maximiliano Seligmann <115603709+mseligmann39@users.noreply.github.com>
2025-08-18 00:08:32 +0200

    Actualizar server.js

--- backend/server.js
+++ backend/server.js
@@ -27,3 +27,5 @@ app.use("/api/content", contentRouter);
 app.use("/api/profile", profileRouter); 
 // Puerto
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server corriendo en puerto &{PORT}'));
